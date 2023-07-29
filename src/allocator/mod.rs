f4c7cc03bc9009350d55d6f58e64682539d0cc3c
AdeThorMiwa <bendamyth@gmail.com>
2023-07-29 16:41:35 +0100

    implementation for fixed size block allocators

--- src/allocator/mod.rs
+++ src/allocator/mod.rs
@@ -7,14 +7,15 @@ use x86_64::{
     VirtAddr,
 };
 
-use self::{linked_list::LinkedListAllocator, lock::Locked};
+use self::{fixed_size_block::FixedSizeBlockAllocator, lock::Locked};
 
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 pub mod lock;
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
